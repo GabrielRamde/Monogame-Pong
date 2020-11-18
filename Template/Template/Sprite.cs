840ea0e2dc759501dcf3f009d9f9d6af3569aacc
GabrielRamde <54704377+GabrielRamde@users.noreply.github.com>
2020-11-18 08:18:01 +0100

    Add files via upload

---
+++ Template/Template/Sprite.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template
+{
+    public class Sprite
+    {
+        protected Texture2D _texture;
+
+        protected Vector2 position;
+        protected Vector2 velocity;
+        protected float speed;
+        protected Input input;
+
+        public Rectangle Rectangle
+        {
+            get
+            {
+                return new Rectangle((int)position.X, (int)position.Y, _texture.Width, _texture.Height);
+            }
+        }
+        public float Speed { get { return speed; } set { speed = value; } }
+        public Input Input { get { return input; } set { input = value; } }
+        public Vector2 Position { get { return position; } set { position = value; } }
+        public Vector2 Velocity { get { return velocity; } set { velocity = value; } }
+        
+    public Sprite(Texture2D texture)
+        {
+            _texture = texture;
+        }
+
+        public virtual void Update(GameTime gameTime, List<Sprite> sprites)
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, position, Color.White);
+        }
+        
+        protected bool IsTouchingLeft(Sprite sprite) //gör så att bollen studsar ifall den träffar bats, tak eller golv
+        {
+            return this.Rectangle.Right + this.velocity.X > sprite.Rectangle.Left &&
+              this.Rectangle.Left < sprite.Rectangle.Left &&
+              this.Rectangle.Bottom > sprite.Rectangle.Top &&
+              this.Rectangle.Top < sprite.Rectangle.Bottom;
+        }
+
+        protected bool IsTouchingRight(Sprite sprite)
+        {
+            return this.Rectangle.Left + this.velocity.X < sprite.Rectangle.Right &&
+              this.Rectangle.Right > sprite.Rectangle.Right &&
+              this.Rectangle.Bottom > sprite.Rectangle.Top &&
+              this.Rectangle.Top < sprite.Rectangle.Bottom;
+        }
+
+        protected bool IsTouchingTop(Sprite sprite)
+        {
+            return this.Rectangle.Bottom + this.velocity.Y > sprite.Rectangle.Top &&
+              this.Rectangle.Top < sprite.Rectangle.Top &&
+              this.Rectangle.Right > sprite.Rectangle.Left &&
+              this.Rectangle.Left < sprite.Rectangle.Right;
+        }
+
+        protected bool IsTouchingBottom(Sprite sprite)
+        {
+            return this.Rectangle.Top + this.velocity.Y < sprite.Rectangle.Bottom &&
+              this.Rectangle.Bottom > sprite.Rectangle.Bottom &&
+              this.Rectangle.Right > sprite.Rectangle.Left &&
+              this.Rectangle.Left < sprite.Rectangle.Right;
+        }
+    }
+}
+
